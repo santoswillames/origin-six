2c6bae6f2e97faebbdd9efa72e62aa7d38c8f811
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-01-19 17:27:46 -0300

    create page section services

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 const links = document.querySelectorAll("nav ul li a");
+const header = document.querySelector("#header");
+const navHight = header.offsetHeight;
 
 function showMenu(element, action) {
   element.addEventListener("click", function () {
@@ -19,3 +21,11 @@ for (const element of toggle) {
 for (const element of links) {
   showMenu(element, "remove");
 }
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
