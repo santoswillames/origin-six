627d602a9a603233cee1d3e305a601af13e380a3
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-01-24 17:52:17 -0300

    create contact

--- main.js
+++ main.js
@@ -43,3 +43,22 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+// Scroll Reveal
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+#home .image, #home .text,
+ #about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimony,
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+);
